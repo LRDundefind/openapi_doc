53984ad0dd4b5b5f1f315668ec9023f7d992dabb
shibeibei <shibeibei@chinamobo.com>
2017-11-16 14:01:37 +0800

    更新config

--- config.example/index.js
+++ config.example/index.js
@@ -1,9 +1,5 @@
-
-'use strict'
-// Template version: 1.1.3
 // see http://vuejs-templates.github.io/webpack for documentation.
-
-const path = require('path')
+var path = require('path')
 
 module.exports = {
   build: {
@@ -11,26 +7,21 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    productionSourceMap: true,
+    assetsPublicPath: '',
+    productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    productionGzipExtensions: ['js', 'css']
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 8082,
-    autoOpenBrowser: true,
+    port: 8082,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    // 地址映射表，可以通过设置将复杂的url简化，还可以解决跨域问题
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
