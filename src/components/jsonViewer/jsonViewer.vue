50a717df0bbd69efdcc47e3e3ef409c4d190d18c
liruidong <286182399@qq.com>
2017-11-23 18:26:36 +0800

    json组件调整

--- src/components/jsonViewer/jsonViewer.vue
+++ src/components/jsonViewer/jsonViewer.vue
@@ -1,15 +1,90 @@
 <template>
   <div class="jsonViewer">
     	<label class="checkbox-inline">
-            <input type="checkbox" id="collapsed" @click="handelJson()">
+            <input type="checkbox" :id="idName+'collapsed'" @click="handelJson()">
             收缩所有的节点 </label>
         <label class="checkbox-inline" @input="handelJson()">
-            <input type="checkbox" id="with-quotes" @click="handelJson()">
+            <input type="checkbox" :id="idName+'with-quotes'" @click="handelJson()">
             为Key添加双引号 </label>
-        <pre id="json-renderer" class="pre-text">格式化Json数据</pre>
+        <pre :id="idName" class="pre-text">格式化Json数据</pre>
   </div>
 
 </template>
+
+<style type="text/css">
+/* Syntax highlighting for JSON objects */
+    ul.json-dict, ol.json-array {
+        list-style-type: none;
+        margin: 0 0 0 1px;
+        border-left: 1px dotted #ccc;
+        padding-left: 2em;
+        font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+        font-size: 13px;
+        line-height: 1.42857143;
+        word-break: break-all;
+    }
+
+    ul.json-dict li {
+        font-weight: bold;
+        color: #CC0000;
+    }
+
+    .json-string {
+        font-weight: bold;
+        color: #007777;
+    }
+
+    .json-literal {
+        color: #AA00AA;
+        font-weight: bold;
+    }
+
+    /* Toggle button */
+    a.json-toggle {
+        position: relative;
+        /*color: inherit;*/
+        text-decoration: none;
+        color: #CC0000;
+        font-weight: bold;
+    }
+
+    a.json-toggle:before {
+        color: #aaa;
+        content: "\25BC"; /* down arrow */
+        position: absolute;
+        display: inline-block;
+        width: 1em;
+        left: -1em;
+    }
+
+    a.json-toggle.collapsed:before {
+        transform: rotate(-90deg); 
+        -ms-transform: rotate(-90deg);
+        -webkit-transform: rotate(-90deg);
+    }
+
+    /* Collapsable placeholder links */
+    a.json-placeholder {
+        color: #aaa;
+        padding: 0 1em;
+        text-decoration: none;
+    }
+
+    a.json-placeholder:hover {
+        text-decoration: underline;
+    }
+
+    .pre-text {
+        counter-reset:section;
+        font-family: Lucida Console, Georgia;
+        font-size: 13px;
+        background-color: #ECECEC;
+        color: #000000;
+        border: solid 1px #CECECE;
+        padding: 3%;
+        width: 97%;
+    }
+</style>
 
 <script>
 
@@ -19,13 +94,14 @@
 
 export default {
   	name: 'json-viewer',
-  	props: ['data'],
+  	props: ['data','idName'],
   	data () {
-  		return{}
+  		return{
+
+  		}
   	}, 
   	mounted() {
         this.handelJson();
-        console.log(this.data.length)
     },
     methods: {
     	handelJson() {
@@ -41,12 +117,11 @@ export default {
                 var input = 'json格式有误';
             }
             var options = {
-                collapsed: $('#collapsed').is(':checked'),
-                withQuotes: $('#with-quotes').is(':checked')
+                collapsed: $('#'+this.idName+'collapsed').is(':checked'),
+                withQuotes: $('#'+this.idName+'with-quotes').is(':checked')
             };
-            $('#json-renderer').jsonViewer(input, options);
+            $('#'+this.idName).jsonViewer(input, options);
         }
-        
     }
 }
 </script>
--- src/views/member/createMember.vue
+++ src/views/member/createMember.vue
@@ -18,8 +18,8 @@
         </el-row>
 
         <!-- 转换json格式组件 -->
-        <json-viewer ref="jsonViewer1" :data="tableData3"></json-viewer>
-        <!-- <json-viewer ref="jsonViewer2" :data="Data" :idName="jsonId.jsonViewer2"></json-viewer> -->
+        <json-viewer :data="Data" :id-name="jsonId.jsonViewer1"></json-viewer>
+        <json-viewer :data="Data1" :id-name="jsonId.jsonViewer2"></json-viewer>
         <el-row>
             <el-col :span="24" style="margin:20px 0">
                 <div><strong class="create">参数说明：</strong></div>
@@ -114,7 +114,7 @@ import jsonViewer from '@/components/jsonViewer/jsonViewer';
                 selectItems: [],
                 jsonId: {
                     jsonViewer1: "Data",
-                    jsonViewer2: "tableData3"
+                    jsonViewer2: "Data1"
                 },
                 
                 types: ['全部', '测试活动', '免费活动', '收费活动'],
@@ -128,6 +128,24 @@ import jsonViewer from '@/components/jsonViewer/jsonViewer';
                         id: '002',
                         title: '"userid"',
                         type: '"张三" ',
+                    }, {
+                        id: '003',
+                        title: '"userid"',
+                        type: '"zhangsan"',
+                    }, {
+                        id: '004',
+                        title: '"english_name"',
+                        type: '"jackzhang"',
+                    }],
+                Data1: [
+                    {
+                        id: '001',
+                        title: '"userid"',
+                        type: '"zhangsan"',
+                    }, {
+                        id: '002',
+                        title: '"userid"',
+                        type: '"李四" ',
                     }, {
                         id: '003',
                         title: '"userid"',
@@ -202,158 +220,5 @@ import jsonViewer from '@/components/jsonViewer/jsonViewer';
 </script>
 
 <style>
-
-    .group {
-        width: 57%;
-        margin-left: 15%;
-    }
-
-    .group .create {
-        font-weight: 700;
-        line-height: 26px;
-    }
-
-    .group .create-content {
-        background: none !important;
-        border: none !important;
-        width: 100%;
-        min-height: 50px;
-        padding: 0;
-        /*border: 1px solid #ddd;*/
-        white-space: pre-wrap;
-        word-wrap: break-word;
-        /*background-color: rgb(247, 248, 251);*/
-    }
-
-    .group .create-content li {
-        list-style-type: decimal;
-        padding-left: 10px;
-        padding-top: 2px;
-        padding-bottom: 2px;
-        color: #333;
-        font-size: 15px;
-    }
-
-    .group .create-content li code {
-        display: inline;
-        max-width: initial;
-        padding: 0;
-        margin: 0;
-        overflow: initial;
-        line-height: inherit;
-        word-wrap: normal;
-        background-color: transparent;
-        border: 0;
-        font-size: 15px;
-    }
-
-    .pln {
-        color: #000;
-    }
-
-    .str {
-        color: #080;
-    }
-
-    .clo, .opn, .pun {
-        color: #660;
-    }
-
-    .group > .head > .el-col > .el-col {
-        padding: 20px 0;
-        border-right: solid 1px #fff;
-    }
-
-    .group .head {
-        text-align: center;
-        color: #fff;
-        font-size: 30px;
-        margin-bottom: 50px;
-    }
-
-    .allActive .head span {
-        font-size: 16px;
-    }
-
-    /* Syntax highlighting for JSON objects */
-    ul.json-dict, ol.json-array {
-        list-style-type: none;
-        margin: 0 0 0 1px;
-        border-left: 1px dotted #ccc;
-        padding-left: 2em;
-        font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
-        font-size: 13px;
-        line-height: 1.42857143;
-        word-break: break-all;
-    }
-
-    ul.json-dict li {
-        font-weight: bold;
-        color: #CC0000;
-    }
-
-    .json-string {
-        /*color: #007777;*/
-        font-weight: bold;
-        color: #007777;
-    }
-
-    .json-literal {
-        color: #AA00AA;
-        font-weight: bold;
-    }
-
-    /* Toggle button */
-    a.json-toggle {
-        position: relative;
-        /*color: inherit;*/
-        text-decoration: none;
-        color: #CC0000;
-        font-weight: bold;
-    }
-
-    a.json-toggle:before {
-        color: #aaa;
-        content: "\25BC"; /* down arrow */
-        position: absolute;
-        display: inline-block;
-        width: 1em;
-        left: -1em;
-    }
-
-    a.json-toggle.collapsed:before {
-        transform: rotate(-90deg); /* Use rotated down arrow, prevents right arrow appearing smaller than down arrow in some browsers */
-        -ms-transform: rotate(-90deg);
-        -webkit-transform: rotate(-90deg);
-    }
-
-    /* Collapsable placeholder links */
-    a.json-placeholder {
-        color: #aaa;
-        padding: 0 1em;
-        text-decoration: none;
-    }
-
-    a.json-placeholder:hover {
-        text-decoration: underline;
-    }
-
-    .pre-text {
-        font-family: Lucida Console, Georgia;
-        font-size: 13px;
-        background-color: #ECECEC;
-        color: #000000;
-        border: solid 1px #CECECE;
-        padding: 3%;
-        width: 97%;
-    }
-
-    .container {
-        padding-right: 15px;
-        padding-left: 15px;
-        margin-right: auto;
-        margin-left: auto;
-    }
-
 
 </style>
