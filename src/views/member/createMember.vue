a9c15bd0351e7161d68b923f09dc70079fd45a4a
liruidong <286182399@qq.com>
2017-11-23 16:44:02 +0800

    修改组件

--- src/views/member/createMember.vue
+++ src/views/member/createMember.vue
@@ -20,19 +20,6 @@
         <!-- 转换json格式组件 -->
         <json-viewer ref="jsonViewer1" :data="tableData3"></json-viewer>
         <!-- <json-viewer ref="jsonViewer2" :data="Data" :idName="jsonId.jsonViewer2"></json-viewer> -->
-
-        <div class="create-content">
-             <textarea class="form-control" id="json-input" @input="handelJson()" rows="15" style="resize: vertical;"
-                       placeholder="请输入json数据">
-             </textarea>
-        </div>
-        <label class="checkbox-inline">
-            <input type="checkbox" id="collapsed" @click="handleCollapsed()">
-            收缩所有的节点 </label>
-        <label class="checkbox-inline" @input="handelJson()">
-            <input type="checkbox" id="with-quotes" @click="handelQuotes()">
-            为Key添加双引号 </label>
-        <pre id="json-renderer" class="pre-text">格式化Json数据</pre>
         <el-row>
             <el-col :span="24" style="margin:20px 0">
                 <div><strong class="create">参数说明：</strong></div>
